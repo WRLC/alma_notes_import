13c7db524c92dc16c83248acdb099d80f3c0dcbe
Tom Boone <boone@wrlc.org>
2020-02-17 16:21:05 -0500

    Get item record via barcode in csv

--- import.py
+++ import.py
@@ -1,6 +1,38 @@
 import csv
 import requests
+import sys
 
-# TODO: Get file from arg
+from settings import *
 
-# TODO: For each row in file, update SCF item with barcode in column 1 to add a note with the value from column 2
+with open(sys.argv[1]) as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    rowNumber = 0
+
+    # Iterate through each row of the CSV
+    for row in csv_reader:
+        barcode = row[0]  # Column 1 = barcode
+        note = row[1]  # Column 2 = value to insert as a note
+
+        # Get item record from barcode via requests
+        try:
+            r = requests.get(ALMA_SERVER + '/almaws/v1/items', params={
+                'apikey': SCF_API_KEY,
+                'item_barcode': barcode,
+                'format': 'json'
+            })
+            itemRec = r.json()
+
+        except requests.exceptions.RequestException as e:  # This is the correct syntax
+            print(e)
+            sys.exit(1)
+
+        # Get IDs from item record for update request params
+        mms_id = itemRec['bib_data']['mms_id']
+        holding_id = itemRec['holding_data']['holding_id']
+        item_pid = itemRec['item_data']['pid']
+
+        # TODO: Update item record with note value from CSV
+
+        # TODO: provide import info as output to command line
+
+        rowNumber = rowNumber + 1
---
+++ settings.template.py
@@ -0,0 +1,2 @@
+ALMA_SERVER = 'https://api-na.hosted.exlibrisgroup.com'
+SCF_API_KEY = '[INSERT API KEY HERE]'
